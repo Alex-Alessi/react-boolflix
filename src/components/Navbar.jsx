ebc150799668ce203b86126b9b87261e0b94f6e1
Alex-Alessi <alex.alessi246@gmail.com>
2024-12-16 22:06:53 +0100

    useState navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,33 @@
 import { NavLink } from "react-router-dom";
 import { AppContext } from "../contexts/CountContext";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSearch = () => {
+    console.log(inputValue);
+  };
+
   return (
     <nav>
-      <div className="container">
+      <div className="Container">
         <div className="navbar">
           <h1>BOOLFLIX</h1>
           <div className="search-bar">
-            <input type="text" placeholder="Cerca..." />
-            <button className="search-button">Cerca</button>
+            <input
+              type="text"
+              value={inputValue}
+              onChange={handleInputChange}
+              placeholder="Cerca..."
+            />
+            <button onClick={handleSearch} className="search-button">
+              Cerca
+            </button>
           </div>
         </div>
       </div>
