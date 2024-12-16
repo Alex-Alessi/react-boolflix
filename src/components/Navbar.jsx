2a1aa470cf97bbcc1343653dac4b7079bb015ab7
Alex-Alessi <alex.alessi246@gmail.com>
2024-12-16 16:49:58 +0100

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,18 @@
 import { NavLink } from "react-router-dom";
+import { AppContext } from "../contexts/CountContext";
 
-export default function Navbar() {}
+export default function Navbar() {
+  return (
+    <nav>
+      <div className="container">
+        <div className="navbar">
+          <h1>BOOLFLIX</h1>
+          <div className="search-bar">
+            <input type="text" placeholder="Cerca..." />
+            <button className="search-button">Cerca</button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
