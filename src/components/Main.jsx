4cd293292c39282bc4b5ef522fa141d479323d34
Alex-Alessi <alex.alessi246@gmail.com>
2024-12-17 16:26:12 +0100

    miniature aggiunte

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,12 +3,13 @@ import { AppContext } from "../contexts/CountContext";
 
 export default function Main() {
   const { movies } = useContext(AppContext);
+  const url_img = "https://image.tmdb.org/t/p/w342";
   return (
     <main>
       <div className="Container">
         {movies.map((movie) => (
           <div key={movie.id} className="card-miniature">
-            <image>{movie.poster_path}</image>
+            <img src={`${url_img}${movie.poster_path}`} />
           </div>
         ))}
       </div>
